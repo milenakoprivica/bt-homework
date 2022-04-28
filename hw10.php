3b4ef90ae37ae5f0e5c7f2f142add1e4a06f736b
“milenakoprivica” <milenakarovski26@gmail.com>
2022-04-28 15:22:00 +0200

    homework upload

---
+++ hw10.php
@@ -0,0 +1,57 @@
+<?php
+// I zadatak
+
+/*
+ $a=200;
+$b=80;
+
+$c = $a + $b;
+echo "Zbir je $c. <br>"
+
+$d = $a*$b;
+echo "Proizvod je $d.<br>";
+
+$f= $a-$b;
+echo "Razlika je $f.<br>";
+
+$m= $a / $b;
+echo "Kolicnik je $m.<br>";
+
+*/
+
+// II zadatak
+
+/*
+
+$meseci=array('Januar','Februar','Mart','April','Maj','Jun');
+
+echo "Sada je $meseci[0] mesec.<br>"
+
+echo "Sada je $meseci[1] mesec.<br>"
+
+echo "Sada je $meseci[2] mesec.<br>"
+
+echo "Sada je $meseci[3] mesec.<br>"
+
+echo "Sada je $meseci[4] mesec.<br>"
+
+echo "Sada je $meseci[5] mesec.<br>"
+
+*/
+
+
+// III zadatak
+
+$x = 6;
+$y = 9;
+$z = 3;
+$s = $z + $y + $z;
+
+echo "Zbir brojeva $x , $y i $z je $s";
+
+
+
+
+
+
+?>
